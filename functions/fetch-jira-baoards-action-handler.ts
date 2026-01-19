813fe028f3f8a4379e313109d02fc1855572c206
devminchev <dev.minchev@proton.me>
2026-01-19 15:34:48 +0000

    jira demo app

--- functions/fetch-jira-baoards-action-handler.ts
+++ functions/fetch-jira-baoards-action-handler.ts
@@ -1,20 +1,4 @@
 import { AppActionRequest, FunctionEventContext, FunctionEventHandler, FunctionTypeEnum } from "@contentful/node-apps-toolkit";
-import { fetchGameDetail, fetchGamesList } from "./apis/externalGameMetadataApi.js";
-import { updateRequestGamePayload, updateRequestSiteGamePayload } from "./utils/entryBuilder.js";
-
-export async function fetchGameDetail(host: string, launchCode: string) {
-  const res = await fetch(`${host}/api/games/details/${launchCode}`, {
-    headers: { 'Content-Type': 'application/json' }
-  });
-  if (!res.ok) {
-    const error = await res.json();
-    const errMessage = `Game Details Source API : ${res.status}-${error.message}`;
-    console.error(errMessage);
-
-    throw new Error(errMessage);
-  };
-  return res.json();
-};
 
 export const fetchJiraBoardActionHandler: FunctionEventHandler<FunctionTypeEnum.AppActionCall> = async (event: AppActionRequest, context: FunctionEventContext) => {
   const { appInstallationParameters } = context;
